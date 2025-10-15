b2337f44c15c2ec251ac489c5c99b6f7e3974b6f
Pahari47 <nachiketapahari00@gmail.com>
2025-10-15 15:04:19 +0530

    structure api design

--- index.js
+++ index.js
@@ -13,5 +13,20 @@ const client = new Client({
 const app = express();
 app.use(express.json());
 
+app.post('/search-messages', async (req, res) => {
+    const {guildId, word, limit} = req.body;
+
+    if(!guildId || !word || !limit) {
+        return res.status(400).json({error: "guildId, word, and limit are required"});
+    }
+
+    try {
+        
+    } catch (error) {
+        
+    }
+})
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`API running on port ${PORT}`));
+client.login(process.env.DISCORD_TOKEN)
