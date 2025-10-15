effa8db2602b1e460ee80ef08550ed11f013dd92
Pahari47 <nachiketapahari00@gmail.com>
2025-10-15 15:00:25 +0530

    solve missing point

--- index.js
+++ index.js
@@ -12,3 +12,6 @@ const client = new Client({
 
 const app = express();
 app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`API running on port ${PORT}`));
